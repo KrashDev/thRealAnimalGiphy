e6634542d116468b3355e5598cab8af8a49734fb
Donny Gabai <mrdgabai@gmail.com>
2017-06-06 00:00:10 -0400

    pausing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,12 @@ function animalBtnFunction(animal){
 		}).done(function(response) {
 			console.log(response);
 			var results = response.data;
-			console.log(response.data)
+			console.log(response.data);
 			
 			for (var i = 0; i < results.length; i++) {
 				
+				$("#gifs-appear-here").attr("data-state", "still");
+
 				var gifDiv = $("<div class='animalButtons'>");
 				var rating = results[i].rating;
 				var p = $("<p>").text("Rating: " + rating);
@@ -30,7 +32,7 @@ function animalBtnFunction(animal){
 				function addAnAnimal() {
 					var btn = document.createElement("BUTTON");
 					document.$("#animalButtons").appendChild(btn);
-					console.log("")
+					console.log("");
 				}
 	
 			}
@@ -57,6 +59,12 @@ $("#addAnimal").on("click", function() {
 	btn.setAttribute('id', datAnimal + 'btn');
 	btn.setAttribute("onclick", 'animalBtnFunction("'+datAnimal+'")');
 	document.getElementById("animalBtns").appendChild(btn);	
+
+	// In order to make it so you can pause/start a gif you need to make 2 versions of the image. 
+	// In our GET request, you can find these 2 versions of the gif image.
+	// In this example the pathing to those images is: response.data.0.images.original and original_still.
+
+
 });
 
 
